62dd3472f9c872b75ff0c38a0e6a5343dadc85b2
gilcierweb <gilcier06@yahoo.com.br>
2025-07-12 03:19:43 -0300

    update image screenshot

--- app-nuxt/components/CategoriesList.vue
+++ app-nuxt/components/CategoriesList.vue
@@ -5,37 +5,37 @@
             <div class="card bg-white shadow-md hover:shadow-lg transition-shadow duration-300">
                 <div class="card-body flex items-center gap-2">
                     <i class="icon-[tabler--shirt-filled] size-10 text-2xl text-primary"></i>
-                    <h5 class="text-lg font-bold">Moda</h5>
+                    <h5 class="text-lg font-bold">Fashion</h5>
                 </div>
             </div>
             <div class="card bg-white shadow-md hover:shadow-lg transition-shadow duration-300">
                 <div class="card-body flex items-center gap-2">
                     <i class="icon-[tabler--device-laptop] size-10 text-2xl text-primary"></i>
-                    <h5 class="text-lg font-bold">Tecnologia</h5>
+                    <h5 class="text-lg font-bold">Technology</h5>
                 </div>
             </div>
             <div class="card bg-white shadow-md hover:shadow-lg transition-shadow duration-300">
                 <div class="card-body flex items-center gap-2">
                     <i class="icon-[tabler--book] size-10 text-2xl text-primary"></i>
-                    <h5 class="text-lg font-bold">Livros</h5>
+                    <h5 class="text-lg font-bold">Books</h5>
                 </div>
             </div>
             <div class="card bg-white shadow-md hover:shadow-lg transition-shadow duration-300">
                 <div class="card-body flex items-center gap-2">
                     <i class="icon-[tabler--home-dot] size-10 text-2xl text-primary"></i>
-                    <h5 class="text-lg font-bold">Casa</h5>
+                    <h5 class="text-lg font-bold">Home</h5>
                 </div>
             </div>
             <div class="card bg-white shadow-md hover:shadow-lg transition-shadow duration-300">
                 <div class="card-body flex items-center gap-2">
                     <i class="icon-[tabler--car] size-10 text-2xl text-primary"></i>
-                    <h5 class="text-lg font-bold">Veículos</h5>
+                    <h5 class="text-lg font-bold">Vehicles</h5>
                 </div>
             </div>
             <div class="card bg-white shadow-md hover:shadow-lg transition-shadow duration-300">
                 <div class="card-body flex items-center gap-2">
                     <i class="icon-[tabler--palette] size-10 text-2xl text-primary"></i>
-                    <h5 class="text-lg font-bold">Arte</h5>
+                    <h5 class="text-lg font-bold">Art</h5>
                 </div>
             </div>
         </div>
